eef1ad49d1ea9a7efb02009ceab28b726eca1241
Bhupinder <bhupinder.global@gmail.com>
2019-03-22 12:32:50 +0530

    added spash screen for ios

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import AppRoutes from './AppRoutes';
 
-export default class App extends Component{
-  render() {
-    return (
-        <AppRoutes/>
-    );
-  }
+//splash screen
+import SplashScreen from 'react-native-splash-screen';
+
+export default class App extends Component {
+    componentDidMount() {
+        SplashScreen.hide()
+    }
+
+    render() {
+        return (
+            <AppRoutes/>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
